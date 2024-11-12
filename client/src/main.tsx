3181e7ecc443045723336c987a1e8a929594ceec
Geoffrey <geoffreyleglise@gmail.com>
2024-11-12 20:18:07 +0100

    create component header/ Add animeList

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/animes",
-    element: <AnimeList />
+    element: <AnimeList />,
   },
   // Try adding a new route! For example, "/about" with an About component
 ]);

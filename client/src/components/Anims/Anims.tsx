238776bc5357df92f0f9465433e3abab980efce2
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-11-15 10:02:28 +0100

    fix bugs

---
+++ client/src/components/Anims/Anims.tsx
@@ -0,0 +1,34 @@
+import Slider from "react-slick";
+
+interface AnimsProps {
+  anims: {
+    id: number;
+    poster_path: string;
+    name: string;
+    overview: string;
+  }[];
+  AnimPath: string;
+}
+
+export default function AnimsMostViewed({ anims, AnimPath }: AnimsProps) {
+  const settings = {
+    className: "center",
+    centerMode: false,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+  };
+
+  return (
+    <section className="slider-container">
+      <Slider {...settings}>
+        {anims.map((anim) => (
+          <article key={anim.id}>
+            <img src={`${AnimPath}${anim.poster_path}`} alt={anim.name} />
+          </article>
+        ))}
+      </Slider>
+    </section>
+  );
+}

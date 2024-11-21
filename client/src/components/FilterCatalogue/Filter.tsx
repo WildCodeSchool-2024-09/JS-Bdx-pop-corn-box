4d4a59af29436147f293ea8fbae11f778c8ee5a5
Geoffrey <geoffreyleglise@gmail.com>
2024-11-21 19:02:03 +0100

    add Filter(no finish) and api key in .env

---
+++ client/src/components/FilterCatalogue/Filter.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+type Categories = {
+    id: number;
+    name: string;
+};
+export default function FilterCatalogue() {
+    const [categories, setCategories] = useState<Categories[]>();
+    useEffect(() => {
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTYxZjNkMjhmYjA0ODQwY2NiNDlkMmQzYjhlZTU1YiIsIm5iZiI6MTczMDczNzc3NS4zOTM1ODY2LCJzdWIiOiI2NzI4ZWFiNzM5NDBjMTIwMmZmN2Q2ODEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.3hTxfLqEU-mn3vqvTs8JvvATPzSiqY67QyMwfhgbGy8",
+            },
+        };
+        fetch("https://api.themoviedb.org/3/genre/movie/list?language=fr", options)
+            .then((res) => res.json())
+            .then((data) => setCategories(data.genres))
+            .catch((err) => console.error(err));
+    });
+    return (
+        <select id="categoryFilter">
+            <option value="all">Toutes les catégories</option>
+            {categories?.map((category) => (
+                <option value={category.id} key={category.id}>
+                    {category.name}
+                </option>
+            ))}
+        </select>
+    );
+}

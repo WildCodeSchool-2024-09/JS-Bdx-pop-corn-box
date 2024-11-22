16875dfe80be449e17ed6e4d27948f0ce9b15997
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-11-22 14:26:17 +0100

    fix home page css

--- client/src/components/PopularMovies/Caroussels.tsx
+++ client/src/components/PopularMovies/Caroussels.tsx
@@ -7,7 +7,7 @@ interface MoviesProps {
     poster_path: string;
     overview: string;
     vote_average: string;
-    vote_count: string;
+    vote_count: number;
     release_date: string;
   }[];
   MoviePath: string;
@@ -18,51 +18,44 @@ export default function MoviesCaroussel({ movies, MoviePath }: MoviesProps) {
     className: "center",
     centerMode: false,
     infinite: true,
-
+    autoplay: true,
     autoplaySpeed: 3000,
     slidesToShow: 1,
     slidesToScroll: 1,
     speed: 500,
     responsive: [
       {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1,
-        },
-      },
-      {
         breakpoint: 480,
         settings: {
-          autoplay: true,
-          autoplaySpeed: 3000,
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          autoplay: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
         },
       },
     ],
   };
 
   return (
-    <section className="slider-container">
+    <main className="slider-container">
       <Slider {...settings}>
         {movies.map((movie) => (
-          <figure className="container1" key={movie.id}>
-            <img
-              className="movie-img"
-              src={`${MoviePath}${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <figcaption className="overview">
-              <p>{movie.overview}</p>
-              <p>{movie.vote_average} ⭐</p>
-              <p>{movie.vote_count}❤️</p>
-              <p>Date de sortie: {movie.release_date}</p>
-            </figcaption>
-          </figure>
+          <section className="home-movieList" key={movie.id}>
+            <figure className="movie-content">
+              <img
+                className="movie-img"
+                src={`${MoviePath}${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <figcaption className="hover-text">
+                <p>{movie.overview}</p>
+                <p>{movie.vote_average} ⭐</p>
+                <p>{movie.vote_count}❤️</p>
+                <p>Date de sortie: {movie.release_date}</p>
+              </figcaption>
+            </figure>
+          </section>
         ))}
       </Slider>
-    </section>
+    </main>
   );
 }
